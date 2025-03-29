dced5a479827c0aed0494df1faa858263d0a2037
saurabh pandey <124060265+saurabhpandey33301@users.noreply.github.com>
2025-03-29 18:17:41 +0530

    n

--- qikpay2/app/api/user/route.ts
+++ qikpay2/app/api/user/route.ts
@@ -1,9 +1,10 @@
-import { getServerSession } from "next-auth"
+
+import { auth } from "@/app/lib/auth";
 import { NextResponse } from "next/server";
-import { authOptions } from "../../lib/auth";
+
 
 export const GET = async () => {
-    const session = await getServerSession(authOptions);
+    const session = await auth()
     
     if (session && session.user) {
         return NextResponse.json({
